6593a5cd583109c8de404cf7062c71ce7361b9c8
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-11-01 10:55:56 +0200

    Add footstep sounds

--- src/Mobile/Assets/Scripts/PlayerController.cs
+++ src/Mobile/Assets/Scripts/PlayerController.cs
@@ -11,13 +11,55 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
 
+    private Vector3 movementDirection;
+
+    // Ref: https://www.youtube.com/watch?v=r1dgRE0GM9A
+    [Header("Footsteps")]
+    [SerializeField]
+    private AudioClip[] footstepAudioClips;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private float stepSpeed;
+
+    private float footstepTimer = 0.0f;
+
+    private int footstepIndex = 0;
+
     private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = transform.right * horizontalInput + transform.forward * verticalInput;
+        movementDirection = transform.right * horizontalInput + transform.forward * verticalInput;
 
         characterController.Move(movementDirection * speed * Time.deltaTime);
+
+        HandleFootsteps();
+    }
+
+    private void HandleFootsteps()
+    {
+        if (movementDirection != Vector3.zero)
+        {
+            footstepTimer -= Time.deltaTime;
+
+            if (footstepTimer <= 0.0f)
+            {
+                audioSource.PlayOneShot(footstepAudioClips[footstepIndex]);
+
+                // Alternate step sounds
+                footstepIndex++;
+
+                if (footstepIndex == footstepAudioClips.Length)
+                {
+                    footstepIndex = 0;
+                }
+
+                footstepTimer = stepSpeed;
+            }
+        }
     }
 }
