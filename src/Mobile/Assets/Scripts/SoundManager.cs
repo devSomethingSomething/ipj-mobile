5e3a69429a43daf0a2b4ce46545a08ebabab7d9c
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-11-07 13:31:28 +0200

    Add say method

--- src/Mobile/Assets/Scripts/SoundManager.cs
+++ src/Mobile/Assets/Scripts/SoundManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
@@ -14,8 +15,33 @@ public class SoundManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    private void Start()
+    {
+        // How to call say method:
+        /* StartCoroutine(Say(new AudioClip[] { 
+            WordManager.Instance.Hello, WordManager.Instance.World
+        })); */
+    }
+
+    // Can still be used for longer sentences if needed
     public void Play(AudioClip audioClip)
     {
         audioSource.PlayOneShot(audioClip);
+    }
+
+    public IEnumerator Say(AudioClip[] audioClips)
+    {
+        foreach (AudioClip audioClip in audioClips)
+        {
+            Play(audioClip);
+
+            while (audioSource.isPlaying)
+            {
+                yield return null;
+            }
+
+            // Small delay between spoken words
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
