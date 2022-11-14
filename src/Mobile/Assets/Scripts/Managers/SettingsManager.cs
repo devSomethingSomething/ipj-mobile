9e0e7ce1c12d36027dd860fd147b85311592a80b
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-11-14 13:24:24 +0200

    Begin moving persistent objects

--- src/Mobile/Assets/Scripts/Managers/SettingsManager.cs
+++ src/Mobile/Assets/Scripts/Managers/SettingsManager.cs
@@ -2,13 +2,7 @@ using UnityEngine;
 
 public class SettingsManager : MonoBehaviour
 {
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
+    private void Awake() => DontDestroyOnLoad(gameObject);
 
-    private void Start()
-    {
-        Application.targetFrameRate = 60;
-    }
+    private void Start() => Application.targetFrameRate = 60;
 }
